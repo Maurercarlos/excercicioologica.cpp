8f9fbd3d548061858c20116bbd42f0f764c072e4
Maurer <162385501+Maurercarlos@users.noreply.github.com>
2024-03-27 00:45:25 +0000

    excercicioologica

--- excercicio2.cpp
+++ excercicio2.cpp
@@ -5,17 +5,22 @@ int main (){
     //Crie um programa que leia um número inteiro do usuário 
     //e verifique se é par ou não. Imprima 1 se for par e 0 se for ímpar.
 
-    int num1;
+    int num;
 
-    //Digitando o primeiro número.
-    printf("Digite o primeiro número: ");
-    scanf("%d", &num1);
+//Digitando o primeiro número.
+printf("Digite o seu número inteiro: ");
+scanf("%d", &num);
 
-    
+if (num % 2 == 0);
 
-    printf("Número")
-
+{
+printf("1\n");
+}  
+else
+{
+printf("0\n");
+}
     return 0;
 }
 
-//Não resolvido.
+//Não resolvido
--- excercicio3.cpp
+++ excercicio3.cpp
@@ -5,7 +5,16 @@ int main(){
 
 int num1, num2, num3; 
 
-printf("");
+printf("Digite o primeiro número: ");
+scanf("%d", num1);
+
+printf("Digite o segundo número: ");
+scanf("%d", num2);
+
+printf("Digite o terceiro número: ")
+scanf("%d", num3);
+
+printf("")
 }
 
 //Não feito em aula, desafio para fazer em casa.
