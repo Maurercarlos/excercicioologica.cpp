b18fa0c4860fa463f7a42a1eabde5a93da57f2a9
Maurer <162385501+Maurercarlos@users.noreply.github.com>
2024-04-03 00:39:27 +0000

    excerciciooLogica

--- ex excercicio6.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-//Escreva um programa que determine se um ano fornecido pelo usuário é bissexto ou não. 
-//Imprima 1 se for bissexto e 0 se não for.
-
-int main (){
-
-    
-}
---
+++ exercicio15.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//O programa deve então determinar se a é maior, menor ou igual a b e imprimir a seguinte mensagem: 
+//Se a for maior que b: "O número a é maior que b.
+//Se a for menor que b: "O número a é menor que b.
+//Se a for igual a b: "Os números a e b são iguais.
+
+int main (){
+
+    int a;
+    int b;
+
+    printf("Digite o seu número: ");
+    scanf("%d", &a);
+
+    printf("%d", (a > b) && (a > b) && (a == b)));
+
+    return 0;
+}
